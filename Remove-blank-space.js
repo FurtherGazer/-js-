19a436c064da67cf125c493d1a92a8f7493687c2
zws <1460639872@qq.com>
2019-02-11 16:21:04 +0800

    添加了阻止页面跳转&去除空格代码段

---
+++ Remove-blank-space.js
@@ -0,0 +1,6 @@
+function Trim(str,is_global){
+    var result;
+    result = str.replace(/(^\s+)|(\s+$)/g,"");
+    if(is_global.toLowerCase()=="g"){result = result.replace(/\s/g,"")}
+    return result;
+}
